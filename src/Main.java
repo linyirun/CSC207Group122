db804dd215bf88fd7b1d46e0213cb853532e551a
Bill Lin <bill10391@gmail.com>
2023-09-30 18:28:42 -0400

    Added Java code for the API call

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=the+weeknd&key=AIzaSyAvLijvxJDqRfGcE6OttWGWc_xaT6ayyK0");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            int status = con.getResponseCode();
+
+            System.out.println("Response Code: " + status);
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+            String input;
+            StringBuilder response = new StringBuilder();
+            while ((input = in.readLine()) != null) {
+                response.append(input);
+                response.append("\n");
+            }
+            in.close();
+            System.out.println(response);
+        } catch (Exception e) {
+
+        }
+    }
+}
